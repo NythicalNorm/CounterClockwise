2faf8737fe9d975d834645cf43f6258320ef2f9c
blockninja124 <64211894+blockninja124@users.noreply.github.com.>
2025-11-24 16:07:23 -0500

    Template be gone

---
+++ src/main/java/com/blockninja/counterclockwise/CounterClockwise.java
@@ -0,0 +1,12 @@
+package com.blockninja.counterclockwise;
+
+import net.minecraftforge.fml.common.Mod;
+
+@Mod(CounterClockwise.MOD_ID)
+public class CounterClockwise {
+    public static final String MOD_ID = "counterclockwise";
+
+    public CounterClockwise() {
+        // Common Code Here
+    }
+}
